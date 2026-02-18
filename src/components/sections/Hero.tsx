e70720070533da6de6cf2f1dd845e158feacbacd
teizred <91955720+teizred@users.noreply.github.com>
2026-02-18 21:25:05 +0100

    style: Adjust responsive styling in Hero section for text alignment, width, and heading font size.

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -6,14 +6,14 @@ export default function Hero() {
             <section id="hero" className="relative h-screen w-full flex items-center justify-center text-white overflow-hidden">
 
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative z-20">
-                <div className="md:w-1/2 mb-10 md:mb-0 fade-in visible">
-                    <h1 className="text-7xl md:text-8xl font-bold mb-6 font-magilo">
+                <div className="w-full md:w-1/2 mb-10 md:mb-0 fade-in visible text-center md:text-left">
+                    <h1 className="text-5xl md:text-8xl font-bold mb-6 font-magilo">
                         Développeur Web Full Stack <span className="text-amber-400">Junior</span>
                     </h1>
                     <p className="text-xl md:text-2xl mb-10 font-montserrat">
                         Je conçois des applications modernes, performantes et centrées sur l’utilisateur, en transformant des idées en solutions concrètes.
                     </p>
-                    <div className="flex flex-wrap items-center gap-4">
+                    <div className="flex flex-wrap items-center justify-center md:justify-start gap-4">
                         <a
                             href="#projects"
                             className="bg-amber-400 text-black font-bold font-magilo px-8 py-3.5 rounded-full hover:bg-amber-300 transition-colors duration-300">
